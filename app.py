426ef0dd66c430054a9efd58cacfd9b4ef01a54b
Justin Huynh <justinhuynh639@gmail.com>
2023-10-18 23:08:16 -0700

    wrote the bot

--- app.py
+++
@@ -1,3 +0,0 @@
-import openai
-
-open.api_key = "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"
---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+
+open.api_key = "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"
+
+# create function that you'll call every time there's a new completion
+def chat_with_gpt(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "user",
+            "content": "prompt"
+            }]
+    )
+
+    # return the user
+    # remove any trailing white space
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+
+        response = chat_with_gpt(user_input)
+        print("Chatbot: ", response)
